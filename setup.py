6975f1068cd9170d4e0fe42d86c3f7561c98e038
SiddharthaKosti <siddharth.kosti@gmail.com>
2024-07-25 14:28:39 +0530

    setup.py file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "GenAi-Doc2QA",
+    version = "0.0.0",
+    author="Siddhartha Kosti",
+    author_email="siddharth.kosti@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
