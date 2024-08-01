80e2acb4bab5fcb3bb7813f0b4a051866e1979e4
SiddharthaKosti <siddharth.kosti@gmail.com>
2024-08-01 22:09:13 +0530

    main app

---
+++ app.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import os
+import shutil
+import dotenv
+
+from src.helper import llm_pipeline
+
+dotenv.load_dotenv()
+
+# Set up OpenAI model
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+st.set_page_config(layout="wide", page_title="GenAI-Doc2QA")
+
+def run_temp_dir():
+    temp_dir = "temp_files"
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+        os.makedirs("temp_files")
+    else:
+        os.makedirs("temp_files")
+
+def check_if_pdf(uploaded_file):
+    if uploaded_file.name.endswith('.pdf'):
+        st.write("PDF file uploaded successfully")
+    else:
+        st.write("Uploaded file is not a PDF. Please choose a PDF file.")
+
+def main():
+    uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
+
+    if uploaded_file is not None:
+        #create a temporary dir
+        run_temp_dir()
+
+        # Save the uploaded file temporarily
+        with open(os.path.join("temp_files", uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        # Get the file path
+        file_path = os.path.join("temp_files", uploaded_file.name)
+
+        answer_generation_chain, ques_list = llm_pipeline(file_path)
+
+        # st.write(ques_list)
+        for question in ques_list:
+            st.write("Question: ", question)
+            answer = answer_generation_chain.run(question)
+            st.write("Answer: ", answer)
+            st.write("--------------------------------------------------\\n\\n")
+            # Save answer to file
+            # with open("answers.txt", "a") as f:
+            #     f.write("Question: " + question + "\\n")
+            #     f.write("Answer: " + answer + "\\n")
+            #     f.write("--------------------------------------------------\\n\\n")
+
+if __name__ == "__main__":
+    main()
